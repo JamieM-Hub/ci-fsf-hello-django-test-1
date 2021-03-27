da311aa4778275f2264aaa25fdaa973593668e22
JamieH-Hub <jamiemckenzie7231@yahoo.co.uk>
2021-03-27 17:36:36 +0000

    course mini-project ready for deployment

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -1,22 +1,20 @@
 from django.test import TestCase
 from .forms import ItemForm
 
-
 class TestItemForm(TestCase):
-
-    def test_item_name_is_required(self):
-        """ Test that the name field is required """
+    
+    def test_item_is_required(self):
         form = ItemForm({'name': ''})
         self.assertFalse(form.is_valid())
         self.assertIn('name', form.errors.keys())
         self.assertEqual(form.errors['name'][0], 'This field is required.')
-
-    def test_done_field_is_not_required(self):
-        """ Test that the done field is NOT required """
+        
+    def test_done_field_is_not_reqiured(self):
         form = ItemForm({'name': 'Test Todo Item'})
         self.assertTrue(form.is_valid())
-
+        
     def test_fields_are_explicit_in_form_metaclass(self):
-        """ Test that fields are specified explicitly on the form """
         form = ItemForm()
         self.assertEqual(form.Meta.fields, ['name', 'done'])
+        
+    
